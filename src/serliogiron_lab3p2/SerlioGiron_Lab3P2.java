a0cfe610ed9094eb483a6c2ac8eba065c1b36bfc
Serlio <gironserlio@gmail.com>
2022-05-06 13:42:35 -0600

    Primer commit

---
+++ src/serliogiron_lab3p2/SerlioGiron_Lab3P2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serliogiron_lab3p2;
+
+/**
+ *
+ * @author usuario
+ */
+public class SerlioGiron_Lab3P2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
